669b8e15262ec8c0ae22448781de6f9300a0610c
ribka muluye <rebekamuluye@gmail.com>
2026-06-06 02:25:45 +0300

    update

--- autoshoopa-frontend/tailwind.config.js
+++ autoshoopa-frontend/tailwind.config.js
@@ -1,11 +1,27 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-orange": {
+          50: "#fff7ed",
+          100: "#ffedd5",
+          200: "#fed7aa",
+          300: "#fdba74",
+          400: "#fb923c",
+          500: "#f97316",
+          600: "#ea580c",
+          700: "#c2410c",
+          800: "#9a3412",
+          900: "#7c2d12",
+        },
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require('@tailwindcss/forms')],
 };
